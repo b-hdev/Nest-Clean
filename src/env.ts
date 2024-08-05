13dabbd1d15eff0b57f3e0f3601b29e4948fe989
b-hdev <bruno.bh969@gmail.com>
2024-08-05 00:47:20 -0300

    z validate

---
+++ src/env.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  PORT: z.coerce.number().optional().default(3333),
+  NODE_ENV: z.string().optional().default('development'),
+});
+
+export type Env = z.infer<typeof envSchema>;
