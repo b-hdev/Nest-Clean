07900ad943c9f1119337d7bb729ea7856e5ddb1b
b-hdev <bruno.bh969@gmail.com>
2024-08-09 01:18:47 -0300

    Core Import

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,34 @@
+import { AnswersRepository } from '../repositories/answers-repository';
+import { Answer } from '../../enterprise/entities/answer';
+import { Either, right } from '@/core/either';
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string;
+  page: number;
+}
+
+type FetchQuestionAnswersUseCaseResponse = Either<
+  null,
+  {
+    answers: Answer[];
+  }
+>;
+export class FetchQuestionAnswersUseCase {
+  constructor(private answerRepository: AnswersRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answerRepository.findManyByQuestionId(
+      questionId,
+      {
+        page,
+      },
+    );
+
+    return right({
+      answers,
+    });
+  }
+}
