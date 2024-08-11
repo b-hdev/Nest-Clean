a617baf4874bd9c32bc5d29166772ab539c96ee2
b-hdev <bruno.bh969@gmail.com>
2024-08-11 19:09:06 -0300

    sla

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
-import { z } from 'zod';
 import { ZodValidationPipe } from '../pipes/zod-validation-pipes';
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
 import { PrismaService } from '@/infra/prisma/prisma.service';
+import { z } from 'zod';
 
 const pageQueryParamSchema = z
   .string()
